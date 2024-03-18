b89482961d2ab1ed34ee242cff1551d4683ac79d
Svyatoslav <62181183+Delgamn@users.noreply.github.com>
2024-03-18 10:58:28 +0300

    Add files via upload

---
+++ NM_Lab1/NM_Lab1/Matrix.cs
@@ -0,0 +1,298 @@
+﻿// Систему уравнений задают шесть векторов: a, b, c, f, p, q
+// a, b, c - векторы для элементов матрицы А, расположенных на нижней кодиагонали, на главной диагонали и на верхней диагонали
+// p, q - векторы для элементов k-й строки и k-ого столбца матрицы А (1 < k < n)
+// f - вектор правой части системы уравнений
+// Вариант 16
+// | | | | | | | |*| | |*|*|
+// | | | | | | | |*| |*|*|*|
+// | | | | | | | |*|*|*|*| |
+// | | | | | | | |*|*|*| | |
+// | | | | | | |*|*|*| | | |
+// | | | | | |*|*|*| | | | |
+// | | | | |*|*|*|*| | | | |
+// | | | |*|*|*| |*| | | | |
+// | | |*|*|*| | |*| | | | |
+// | |*|*|*| | | |*| | | | |
+// |*|*|*| | | | |*| | | | |
+// |*|*| | | | | |*| | | | |
+
+//| | | | | | |*| |*|*|
+//| | | | | | |*|*|*|*|
+//| | | | | | |*|*|*| |
+//| | | | | |*|*|*| | |
+//| | | | |*|*|*| | | |
+//| | | |*|*|*|*| | | |
+//| | |*|*|*| |*| | | |
+//| |*|*|*| | |*| | | |
+//|*|*|*| | | |*| | | |
+//|*|*| | | | |*| | | |
+
+
+
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.Design;
+using System.Data;
+using System.Transactions;
+
+class RandomGenerator
+{
+    private decimal left;
+    private decimal right;
+    Random random = new Random();
+
+    public RandomGenerator(decimal left, decimal right)
+    {
+        this.left = left;
+        this.right = right;
+
+    }
+
+    public decimal GetRandomDecimalValue()
+    {
+        var item = new decimal(random.NextDouble());
+        return left + item * (right - left);
+    }
+}
+
+class Matrix
+{
+    public decimal[,] matrix;
+    public decimal[] f;
+    public decimal[] f_one;
+    public decimal[] x;
+    public decimal[] x_one;
+    public decimal[] x_generated;
+    public int size;
+    public int k;
+    public decimal inaccuracy;
+    public decimal accuracy;
+
+    public Matrix(int size, int k)
+    {
+        matrix = new decimal[size, size];
+        f = new decimal[size];
+        f_one = new decimal[size];
+        x = new decimal[size]; // массив полученных решений
+        x_one = new decimal[size]; // массив полученных единичных решений
+        x_generated = new decimal[size]; // массив сгенерированных решений
+        this.size = size;
+        this.k = k;
+    }
+
+    public void Generate(decimal left, decimal right)
+    {
+        RandomGenerator gen = new RandomGenerator(left, right);
+
+        for (int col = 0; col < size; col++)
+        {
+            x_generated[col] = gen.GetRandomDecimalValue();
+        }
+
+        for (int row = 0; row < size; row++)
+        {
+            for (int col = 0; col < size; col++)
+            {
+                if (col == k - 1)
+                {
+                    matrix[row, col] = gen.GetRandomDecimalValue();
+                }
+                else if (col == size - 2 - row || col == size - 1 - row || col == size - row)
+                {
+                    matrix[row, col] = gen.GetRandomDecimalValue();
+                }
+                else
+                {
+                    matrix[row, col] = 0;
+                }
+            }
+        }
+
+        for (int row = 0; row < size; row++)
+        {
+            for (int col = 0; col < size; col++)
+            {
+                f[row] += x_generated[col] * matrix[row, col];
+                f_one[row] += matrix[row, col];
+            }
+        }
+    }
+
+    public void DivideLine(int rowIndex)
+    {
+        decimal b_element = matrix[rowIndex, size - 1 - rowIndex];
+        for (int col = 0; col < size; col++)
+        {
+            matrix[rowIndex, col] /= b_element;
+        }
+        f[rowIndex] /= b_element;
+        f_one[rowIndex] /= b_element;
+        //this.PrintToConsole();
+    }
+
+    public void Subtract(int firstRow, int secondRow) // из второй вычитается первая, умноженная на коэффициент во второй
+    {
+        decimal koef = matrix[secondRow, size - 1 - firstRow];
+        for (int col = 0; col < size; col++)
+        {
+            matrix[secondRow, col] -= matrix[firstRow, col] * koef;
+        }
+        f[secondRow] -= f[firstRow] * koef;
+        f_one[secondRow] -= f_one[firstRow] * koef;
+        //this.PrintToConsole();
+    }
+
+    public void InputFromFile(string path)
+    {
+        using (StreamReader reader = new StreamReader(path))
+        {
+            for (int row = 0; row < size; row++)
+            {
+                string line = reader.ReadLine();
+                string[] values = line.Split('\t');
+
+                for (int col = 0; col < size; col++)
+                {
+                    matrix[row, col] = decimal.Parse(values[col]);
+                    f_one[row] += matrix[row, col];
+                }
+            }
+
+            reader.ReadLine();
+
+            for (int i = 0; i < size; i++)
+            {
+                f[i] = decimal.Parse(reader.ReadLine());
+            }
+        }
+    }
+
+
+    public void PrintToConsole()
+    {
+        Console.WriteLine("----------------------------------------------------------------------------------");
+        for (int row = 0; row < size; row++)
+        {
+            for (int col = 0; col < size; col++)
+            {
+                if (matrix[row, col] == 0)
+                {
+                    Console.Write(string.Format(" {0:f16} ", 0.0));
+                }
+                else
+                {
+                    if (matrix[row, col] > 0)
+                    {
+                        Console.Write(' ');
+                    }
+
+                    Console.Write(string.Format("{0:f16} ", matrix[row, col]));
+
+                }
+                if (col < size - 1)
+                {
+                    Console.Write(" ");
+                }
+            }
+
+            Console.WriteLine($" = {f[row]:f16}");
+        }
+        Console.WriteLine("----------------------------------------------------------------------------------");
+        Console.WriteLine();
+
+    }
+    public void PrintToFile(string path)
+    {
+        using (StreamWriter writer = new StreamWriter(path))
+        {
+            for (int row = 0; row < size; row++)
+            {
+                for (int col = 0; col < size; col++)
+                {
+                    if (matrix[row, col] == 0)
+                    {
+                        writer.Write(string.Format("{0:f16} ", 0.0));
+                    }
+                    else
+                    {
+                        writer.Write(string.Format("{0:f16} ", matrix[row, col]));
+                    }
+
+                    if (col < size - 1)
+                    {
+                        writer.Write(" ");
+                    }
+                }
+
+                writer.WriteLine($" = {f[row]:f16}");
+            }
+            writer.WriteLine();
+        }
+    }
+
+    public void PrintSolutionsToFile(string path)
+    {
+        using (StreamWriter writer = new StreamWriter(path))
+        {
+            writer.WriteLine("Solutions:");
+            for (int row = 0; row < size; row++)
+            {
+                writer.Write("x" + (row + 1) + $" = {x[row]:f16}");
+                writer.WriteLine();
+            }
+            writer.WriteLine();
+        }
+    }
+
+    public void PrintGeneratedSolutionsToFile(string path)
+    {
+        using (StreamWriter writer = new StreamWriter(path, true))
+        {
+            writer.WriteLine("Generated solutions:");
+            for (int row = 0; row < size; row++)
+            {
+                writer.Write("x" + (row + 1) + $" = {x_generated[row]:f16}");
+                writer.WriteLine();
+            }
+            writer.WriteLine();
+        }
+    }
+
+    public void AccuracyTest(decimal[] _x)
+    {
+        accuracy = 0;
+        for (int i = 0; i < size; i++)
+        {
+            accuracy = Math.Max(Math.Abs(_x[i] - 1.0M), accuracy);
+        }
+    }
+
+    public void InaccuracyTest(decimal[] _x, decimal[] _x_expect)
+    {
+        inaccuracy = 0;
+        for (int i = 0; i < size; i++)
+        {
+            inaccuracy = Math.Max(Math.Abs((_x[i] - _x_expect[i]) / Math.Max(1, _x[i])), inaccuracy);
+        }
+    }
+
+    public void PrintAccuracy(string path)
+    {
+        using (StreamWriter writer = new StreamWriter(path, true))
+        {
+            writer.WriteLine($"Accuracy: {accuracy:e}");
+        }
+    }
+
+    public void PrintInaccuracy(string path)
+    {
+        using (StreamWriter writer = new StreamWriter(path, true))
+        {
+            writer.WriteLine($"Inaccuracy: {inaccuracy:e}");
+        }
+    }
+}
---
+++ NM_Lab1/NM_Lab1/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Net.Http.Headers;
+using System.IO;
+using System.Drawing;
+
+class Program
+{
+
+
+    public static void FirstStep(Matrix m)
+    {
+        for (int row = 0; row < m.size - 1; row++)
+        {
+            m.DivideLine(row);
+            m.Subtract(row, row + 1);
+        }
+
+        m.DivideLine(m.size - 1);
+        SecondStep(m);
+    }
+
+    public static void SecondStep(Matrix m)
+    {
+        for (int row = m.size - 1; row > 0; row--)
+        {
+            m.Subtract(row, row - 1);
+        }
+        ThirdStep(m);
+    }
+
+    public static void ThirdStep(Matrix m)
+    {
+        int k_row = m.size - m.k;
+        m.DivideLine(k_row);
+
+        for (int row = 0; row < k_row; ++row)
+        {
+            m.Subtract(k_row, row);
+        }
+
+        for (int row = m.size - 1; row > k_row; --row)
+        {
+            m.Subtract(k_row, row);
+        }
+
+        for (int row = 0; row < m.size; row++)
+        {
+            m.x[row] = m.f[m.size - 1 - row];
+            m.x_one[row] = m.f_one[m.size - 1 - row];
+        }
+    }
+
+
+    public static int InputSize(string path)
+    {
+        int size = 0;
+        using (StreamReader reader = new StreamReader(path))
+        {
+            string line = reader.ReadLine();
+            if (line != null)
+            {
+                size = line.Split('\t').Length;
+            }
+        }
+        return size;
+    }
+
+    public static void Tests(string inputpath, string outputpath)
+    {
+        bool flag = false;
+        StreamReader reader = new StreamReader(inputpath);
+        while (!reader.EndOfStream)
+        {
+            string line = reader.ReadLine();
+            string[] numbers = line.Split(' ');
+            int size = int.Parse(numbers[0]);
+            int k = size / 2 + 2;
+            decimal left = decimal.Parse(numbers[1]);
+            decimal right = decimal.Parse(numbers[2]);
+
+            Matrix matrix = new Matrix(size, k);
+            matrix.Generate(left, right);
+            FirstStep(matrix);
+            matrix.AccuracyTest(matrix.x_one);
+            matrix.InaccuracyTest(matrix.x, matrix.x_generated);
+
+            using (StreamWriter writer = new StreamWriter(outputpath, flag))
+            {
+                writer.WriteLine($"Size: {size}; Left: {left}; Right: {right}; Inaccuracy: {matrix.inaccuracy:e}; Accuracy: {matrix.accuracy:e}");
+                flag = true;
+            }
+        }
+    }
+    static void Main()
+    {
+        bool menu = true;
+        while (menu)
+        {
+            Console.WriteLine("1) Решить СЛАУ из файла;\n2) Решить сгенерированную СЛАУ;\n3) Набор тестов;\n4) Выход");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    int size = InputSize("D:\\NM_Lab1\\NM_Lab1\\variant.txt");
+                    Matrix static_m = new Matrix(size, size / 2 + 2);
+                    static_m.InputFromFile("D:\\NM_Lab1\\NM_Lab1\\variant.txt");
+                    static_m.PrintToFile("D:\\NM_Lab1\\NM_Lab1\\initial_static_matrix.txt");
+                    FirstStep(static_m);
+                    static_m.PrintSolutionsToFile("D:\\NM_Lab1\\NM_Lab1\\static_matrix_solutions.txt");
+                    static_m.AccuracyTest(static_m.x_one);
+                    static_m.PrintAccuracy("D:\\NM_Lab1\\NM_Lab1\\static_matrix_solutions.txt");
+                    break;
+                case "2":
+                    Console.Write("Размер матрицы: ");
+                    int n = Convert.ToInt32(Console.ReadLine());
+                    int k = n / 2 + 2;
+                    Matrix generated_m = new Matrix(n, k);
+                    generated_m.Generate(-1, 1);
+                    generated_m.PrintToFile("D:\\NM_Lab1\\NM_Lab1\\initial_generated_matrix.txt");
+                    FirstStep(generated_m);
+                    generated_m.PrintSolutionsToFile("D:\\NM_Lab1\\NM_Lab1\\generated_matrix_solutions.txt");
+                    generated_m.PrintGeneratedSolutionsToFile("D:\\NM_Lab1\\NM_Lab1\\generated_matrix_solutions.txt");
+                    generated_m.AccuracyTest(generated_m.x_one);
+                    generated_m.InaccuracyTest(generated_m.x, generated_m.x_generated);
+                    generated_m.PrintAccuracy("D:\\NM_Lab1\\NM_Lab1\\generated_matrix_solutions.txt");
+                    generated_m.PrintInaccuracy("D:\\NM_Lab1\\NM_Lab1\\generated_matrix_solutions.txt");
+                    break;
+                case "3":
+                    Tests("D:\\NM_Lab1\\NM_Lab1\\tests.txt", "D:\\NM_Lab1\\NM_Lab1\\result_tests.txt");
+                    break;
+                case "4":
+                    menu = false;
+                    break;
+                default:
+                    Console.WriteLine("Неправильный ввод");
+                    break;
+            }
+        }
+    }
+}
